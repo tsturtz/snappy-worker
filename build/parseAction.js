b75c2477a9bdebb01089cecc2cc13576c76f4279
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2020-09-16 23:36:29 +0300

    Remove build dir

--- build/parseAction.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const parseAction = (message) => {
-    return message
-        .toLowerCase()
-        .replace(/\[(.*?)\]/g, "")
-        .trim();
-};
-exports.default = parseAction;
