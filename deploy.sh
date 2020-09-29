607f7aa9d4a2e6a08388487fc3a54ad4be5aed4d
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2020-09-29 18:43:12 +0300

    fix deploy.yml

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm -rf build
-npm run build
+#copy build
 tar czf build.tar.gz public build
-scp build.tar.gz root@82.148.19.60:~
+scp build.tar.gz deployer@82.148.19.60:~
 rm -rf build.tar.gz
 
-ssh root@82.148.19.60 << 'ENDSSH'
+#running
+ssh deployer@82.148.19.60 << 'ENDSSH'
 pm2 stop snappy-worker
 rm -rf snappy-worker
 mkdir snappy-worker
